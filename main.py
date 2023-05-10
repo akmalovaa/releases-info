67009c64a155e281749e9b226e994293e11680b0
Artur Akmalov <artur@akmalov.com>
2023-05-10 14:30:39 +0500

    reduced main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ import github_releases
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-def _fetch_result_yaml():
+def _fetch_result_yaml() -> dict:
     """Fetch result.yaml."""
     with open("result.yaml") as stream:
         try:
             result_yaml = yaml.safe_load(stream)
+            result_yaml = result_yaml['services']
         except yaml.YAMLError as exc:
             logging.error(f"YAML error in result.yaml {exc}")
             return {}
@@ -23,21 +24,17 @@ def _fetch_result_yaml():
 
 @app.route('/', methods=['GET'])
 def index():
-    try:
-        context = _fetch_result_yaml()['services']
-    except:
-        context = {}
-    return render_template('index.html', context=context)
+    return render_template('index.html', context=_fetch_result_yaml())
 
 
 @app.route('/config', methods=['GET'])
 def config():
     try:
-        context = github_releases.fetch_config_yaml()
-        context = context['services']
+        config_yaml = github_releases.fetch_config_yaml()
+        config_yaml = config_yaml['services']
     except:
-        context = {}
-    return render_template('config.html', context=context)
+        config_yaml = {}
+    return render_template('config.html', context=config_yaml)
 
 
 @app.route('/result', methods=['GET', 'POST'])
@@ -45,11 +42,7 @@ def result():
     if request.method == 'POST':
         github_releases.generate_result_yaml()
         logging.info("Generate result.yaml")
-    try:
-        context = _fetch_result_yaml()['services']
-    except:
-        context = {}
-    return render_template('result.html', context=context)
+    return render_template('result.html', context=_fetch_result_yaml())
 
 
 if __name__ == '__main__':
